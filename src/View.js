09a2f95fa5635d989a70b21b3f993ccd049d64ba
Trung Ly <trung.ly@cit3-501m-01.local>
2019-08-20 14:07:46 +1200

    Fix displaying views to remove inactive views from the DOM

--- src/View.js
+++ src/View.js
@@ -2,19 +2,21 @@ import React, {Component} from 'react';
 
 class  View extends Component {
 
-  render(){
+  	render(){
 
-    var {className, children, viewName, activeView} = this.props;
+	    var {className, children, viewName, activeView} = this.props;
 
-    var newClassName = (viewName == activeView) ? 'view active '+className : 'view '+className;
+	    var newClassName = 'view '+className;
 
-    return (
+	    return (viewName == activeView) ? (
 
-      <div className={newClassName}>
-        {children}
-      </div>
-    );
-  }
+		      	<div className={newClassName}>
+		        	{children}
+		      	</div>
+		      	
+		    ) :null
+
+  	}
 }
 
 export default View;
